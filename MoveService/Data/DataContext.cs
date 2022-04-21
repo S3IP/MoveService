488ccc496a0ebcf461c5d3f723a9023954741350
Tim Meijvogel <timmeijvogel2003@gmail.com>
2022-04-21 11:34:07 +0200

    backup

--- MoveService/Data/DataContext.cs
+++ MoveService/Data/DataContext.cs
@@ -1,7 +1,15 @@
-﻿namespace MoveService.Data
+﻿using Microsoft.EntityFrameworkCore;
+using MoveService.Models;
+
+namespace MoveService.Data
 {
     public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
 
+        }
+
+        public DbSet<Move> Moves { get; set; }
     }
 }
